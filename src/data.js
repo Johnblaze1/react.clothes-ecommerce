cb87867d10127bf45fd952c0bc08641000fcf2bb
Johnblaze1 <abayomijohn26@gmail.com>
2019-12-30 10:57:41 +0100

    second

---
+++ src/data.js
@@ -0,0 +1,208 @@
+export const storeProducts =[
+    {
+        id: 1,
+        title: "Google Pixel - Black",
+        img: "img/product-1.jpeg",
+        price: 10,
+        company: "GOOGLE",
+        info: 
+            "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum provident, commodi a delectus vitae, quo iusto reprehenderit quod optio ipsum dolores accusamus? Perspiciatis inventore culpa vel consequuntur debitis consectetur facilis?",
+        inCart: false,
+        Count: 0,
+        total: 0
+    },
+    {
+        id: 2,
+        title: "Collar T-Shirt",
+        img: "img/product-2.jpeg",
+        price: 12,
+        company: "ttttttttttt",
+        info:
+            "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum provident, commodi a delectus vitae, quo iusto reprehenderit quod optio ipsum dolores accusamus? Perspiciatis inventore culpa vel consequuntur debitis consectetur facilis?",
+        inCart: false,
+        Count: 0,
+        total: 0
+    },
+    {
+        id: 3,
+        title: "Google Pixel - Black",
+        img: "img/product-3.jpeg",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum provident, commodi a delectus vitae, quo iusto reprehenderit quod optio ipsum dolores accusamus? Perspiciatis inventore culpa vel consequuntur debitis consectetur facilis?",
+        inCart: false,
+        Count: 0,
+        total: 0
+    },
+
+    {
+        id: 4,
+        title: "Google Pixel - Black",
+        img: "img/product-4.jpeg",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum provident, commodi a delectus vitae, quo iusto reprehenderit quod optio ipsum dolores accusamus? Perspiciatis inventore culpa vel consequuntur debitis consectetur facilis?",
+        inCart: false,
+        Count: 0,
+        total: 0
+    },
+    {
+        id: 5,
+        title: "Google Pixel - Black",
+        img: "img/product-5.jpeg",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum provident, commodi a delectus vitae, quo iusto reprehenderit quod optio ipsum dolores accusamus? Perspiciatis inventore culpa vel consequuntur debitis consectetur facilis?",
+        inCart: false,
+        Count: 0,
+        total: 0
+    },
+    {
+        id: 6,
+        title: "Google Pixel - Black",
+        img: "img/product-6.jpeg",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum provident, commodi a delectus vitae, quo iusto reprehenderit quod optio ipsum dolores accusamus? Perspiciatis inventore culpa vel consequuntur debitis consectetur facilis?",
+        inCart: false,
+        Count: 0,
+        total: 0
+    },
+    {
+        id: 7,
+        title: "Google Pixel - Black",
+        img: "img/product-7.jpeg",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum provident, commodi a delectus vitae, quo iusto reprehenderit quod optio ipsum dolores accusamus? Perspiciatis inventore culpa vel consequuntur debitis consectetur facilis?",
+        inCart: false,
+        Count: 0,
+        total: 0
+    },
+    {
+        id: 8,
+        title: "Google Pixel - Black",
+        img: "img/product-8.jpeg",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum provident, commodi a delectus vitae, quo iusto reprehenderit quod optio ipsum dolores accusamus? Perspiciatis inventore culpa vel consequuntur debitis consectetur facilis?",
+        inCart: false,
+        Count: 0,
+        total: 0
+    },
+    {
+        id: 9,
+        title: "Google Pixel - Black",
+        img: "img/product-9.jpeg",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum provident, commodi a delectus vitae, quo iusto reprehenderit quod optio ipsum dolores accusamus? Perspiciatis inventore culpa vel consequuntur debitis consectetur facilis?",
+        inCart: false,
+        Count: 0,
+        total: 0
+    },
+    {
+        id: 10,
+        title: "Google Pixel - Black",
+        img: "img/product-10.jpeg",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum provident, commodi a delectus vitae, quo iusto reprehenderit quod optio ipsum dolores accusamus? Perspiciatis inventore culpa vel consequuntur debitis consectetur facilis?",
+        inCart: false,
+        Count: 0,
+        total: 0
+    },
+    {
+        id: 11,
+        title: "Google Pixel - Black",
+        img: "img/product-11.jpeg",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum provident, commodi a delectus vitae, quo iusto reprehenderit quod optio ipsum dolores accusamus? Perspiciatis inventore culpa vel consequuntur debitis consectetur facilis?",
+        inCart: false,
+        Count: 0,
+        total: 0
+    },
+    {
+        id: 12,
+        title: "Google Pixel - Black",
+        img: "img/product-12.jpeg",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum provident, commodi a delectus vitae, quo iusto reprehenderit quod optio ipsum dolores accusamus? Perspiciatis inventore culpa vel consequuntur debitis consectetur facilis?",
+        inCart: false,
+        Count: 0,
+        total: 0
+    },
+    {
+        id: 13,
+        title: "Google Pixel - Black",
+        img: "img/product-13.jpeg",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum provident, commodi a delectus vitae, quo iusto reprehenderit quod optio ipsum dolores accusamus? Perspiciatis inventore culpa vel consequuntur debitis consectetur facilis?",
+        inCart: false,
+        Count: 0,
+        total: 0
+    },
+    {
+        id: 14,
+        title: "Google Pixel - Black",
+        img: "img/product-14.jpeg",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum provident, commodi a delectus vitae, quo iusto reprehenderit quod optio ipsum dolores accusamus? Perspiciatis inventore culpa vel consequuntur debitis consectetur facilis?",
+        inCart: false,
+        Count: 0,
+        total: 0
+    },
+    {
+        id: 15,
+        title: "Google Pixel - Black",
+        img: "img/product-15.jpeg",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum provident, commodi a delectus vitae, quo iusto reprehenderit quod optio ipsum dolores accusamus? Perspiciatis inventore culpa vel consequuntur debitis consectetur facilis?",
+        inCart: false,
+        Count: 0,
+        total: 0
+    },
+    {
+        id: 16,
+        title: "Google Pixel - Black",
+        img: "img/product-16.jpeg",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum provident, commodi a delectus vitae, quo iusto reprehenderit quod optio ipsum dolores accusamus? Perspiciatis inventore culpa vel consequuntur debitis consectetur facilis?",
+        inCart: false,
+        Count: 0,
+        total: 0
+    },
+]
+
+export const detailProduct = {
+    id: 1,
+    title: "Google Pixel -Black",
+    img: "img/product-1.jpeg",
+    price: 10,
+    company: "google",
+    info: "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum provident, commodi a delectus vitae, quo iusto reprehenderit quod optio ipsum dolores accusamus? Perspiciatis inventore culpa vel consequuntur debitis consectetur facilis?",
+    inCart: false,
+    count:0,
+    total: 0
+
+}
